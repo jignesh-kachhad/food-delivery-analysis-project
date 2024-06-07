d6054b61a44a16d4c88bca26b7030edf9b38066d
Jignesh <jignesh2081@gmail.com>
2024-06-07 16:50:03 +0530

    commit

--- dags/dim_load_dag.py
+++ dags/dim_load_dag.py
@@ -3,7 +3,9 @@ from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOp
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
-from airflow.operators.dagrun_operator import TriggerDagRunOperator
+# from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
 
 default_args = {
     "owner": "airflow",
@@ -26,39 +28,39 @@ with DAG(
     # Create schema if it doesn't exist
     create_schema = PostgresOperator(
         task_id="create_schema",
-        postgres_conn_id="redshift_connection_id",
+        postgres_conn_id="redshift_default",
         sql="CREATE SCHEMA IF NOT EXISTS food_delivery_datamart;",
     )
 
     # Drop tables if they exist
     drop_dimCustomers = PostgresOperator(
         task_id="drop_dimCustomers",
-        postgres_conn_id="redshift_connection_id",
+        postgres_conn_id="redshift_default",
         sql="DROP TABLE IF EXISTS food_delivery_datamart.dimCustomers;",
     )
 
     drop_dimRestaurants = PostgresOperator(
         task_id="drop_dimRestaurants",
-        postgres_conn_id="redshift_connection_id",
+        postgres_conn_id="redshift_default",
         sql="DROP TABLE IF EXISTS food_delivery_datamart.dimRestaurants;",
     )
 
     drop_dimDeliveryRiders = PostgresOperator(
         task_id="drop_dimDeliveryRiders",
-        postgres_conn_id="redshift_connection_id",
+        postgres_conn_id="redshift_default",
         sql="DROP TABLE IF EXISTS food_delivery_datamart.dimDeliveryRiders;",
     )
 
     drop_factOrders = PostgresOperator(
         task_id="drop_factOrders",
-        postgres_conn_id="redshift_connection_id",
+        postgres_conn_id="redshift_default",
         sql="DROP TABLE IF EXISTS food_delivery_datamart.factOrders;",
     )
 
     # Create dimension and fact tables
     create_dimCustomers = PostgresOperator(
         task_id="create_dimCustomers",
-        postgres_conn_id="redshift_connection_id",
+        postgres_conn_id="redshift_default",
         sql="""
             CREATE TABLE food_delivery_datamart.dimCustomers (
                 CustomerID INT PRIMARY KEY,
@@ -73,7 +75,7 @@ with DAG(
 
     create_dimRestaurants = PostgresOperator(
         task_id="create_dimRestaurants",
-        postgres_conn_id="redshift_connection_id",
+        postgres_conn_id="redshift_default",
         sql="""
             CREATE TABLE food_delivery_datamart.dimRestaurants (
                 RestaurantID INT PRIMARY KEY,
@@ -87,7 +89,7 @@ with DAG(
 
     create_dimDeliveryRiders = PostgresOperator(
         task_id="create_dimDeliveryRiders",
-        postgres_conn_id="redshift_connection_id",
+        postgres_conn_id="redshift_default",
         sql="""
             CREATE TABLE food_delivery_datamart.dimDeliveryRiders (
                 RiderID INT PRIMARY KEY,
@@ -102,7 +104,7 @@ with DAG(
 
     create_factOrders = PostgresOperator(
         task_id="create_factOrders",
-        postgres_conn_id="redshift_connection_id",
+        postgres_conn_id="redshift_default",
         sql="""
             CREATE TABLE food_delivery_datamart.factOrders (
                 OrderID INT PRIMARY KEY,
@@ -128,7 +130,7 @@ with DAG(
         s3_key="dims/dimCustomers.csv",
         copy_options=["CSV", "IGNOREHEADER 1", "QUOTE as '\"'"], 
         aws_conn_id="aws_default",
-        redshift_conn_id="redshift_connection_id",
+        redshift_conn_id="redshift_default",
     )
 
     load_dimRestaurants = S3ToRedshiftOperator(
@@ -139,7 +141,7 @@ with DAG(
         s3_key="dims/dimRestaurants.csv",
         copy_options=["CSV", "IGNOREHEADER 1", "QUOTE as '\"'"],
         aws_conn_id="aws_default",
-        redshift_conn_id="redshift_connection_id",
+        redshift_conn_id="redshift_default",
     )
 
     load_dimDeliveryRiders = S3ToRedshiftOperator(
@@ -150,7 +152,7 @@ with DAG(
         s3_key="dims/dimDeliveryRiders.csv",
         copy_options=["CSV", "IGNOREHEADER 1", "QUOTE as '\"'"],
         aws_conn_id="aws_default",
-        redshift_conn_id="redshift_connection_id", 
+        redshift_conn_id="redshift_default", 
     )
 
     trigger_spark_streaming_dag = TriggerDagRunOperator(
